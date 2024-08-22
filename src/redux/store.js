b10f941250e200f71485fa2ab18bafcd22904fa2
Aditya <aditya.vishwabharati@gmail.com>
2024-08-22 11:04:40 +0530

    updated reducers in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import projectReducer from './projectSlice';
+import tasksReducer from './tasksSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     project: projectReducer,
+    tasks: tasksReducer,
   },
 });
