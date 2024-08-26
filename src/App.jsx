1ee5b7bb068524c8de361af0768a280e5e38121c
Aditya <aditya.vishwabharati@gmail.com>
2024-08-26 11:45:02 +0530

    updated routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignUp from './pages/Signup';
 import Login from './pages/Login';
 import ChooseProjectPage from './pages/ChooseProjectPage'; // Assuming you will create this
 import Dashboard from './pages/Dashboard';
+import LandingPage from './pages/LandingPage';
 
 const App = () => (
   <Router>
     <Routes>
+      <Route path="/" element={<LandingPage />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
       <Route path="/chooseprojectpage" element={<ChooseProjectPage />} />
