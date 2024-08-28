cea77e05d85d539e536ea33d9a5a92a895858c2e
Aditya <aditya.vishwabharati@gmail.com>
2024-08-28 17:01:59 +0530

    basic updation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
 // src/App.jsx or src/Routes.jsx
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import SignUp from './pages/Signup';
 import Login from './pages/Login';
 import ChooseProjectPage from './pages/ChooseProjectPage'; // Assuming you will create this
 import Dashboard from './pages/Dashboard';
 import LandingPage from './pages/LandingPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const App = () => (
+const App = () => {
+  const location = useLocation();
+
+  // Define paths where Header and Footer should not appear
+  const noHeaderFooterPaths = ['/dashboard'];
+  const noHeaderPaths = ['/', '/dashboard']
+
+  return (
+    <>
+
+    
+      {!noHeaderPaths.includes(location.pathname) && <Header />}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chooseprojectpage" element={<ChooseProjectPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+      {!noHeaderFooterPaths.includes(location.pathname) && <Footer />}
+    </>
+  );
+};
+
+const AppWrapper = () => (
   <Router>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/chooseprojectpage" element={<ChooseProjectPage />} />
-      <Route path="/dashboard" element={<Dashboard/>} />
-      {/* Add other routes here */}
-    </Routes>
+    <App />
   </Router>
 );
 
-export default App;
+export default AppWrapper;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AppWrapper from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <AuthProvider>
-      <App />
+      <AppWrapper />
     </AuthProvider>
   </Provider>,
 );
