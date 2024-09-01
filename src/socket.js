55897c24ffde214b9f84b9e9c8acc2fc00367f68
Aditya <aditya.vishwabharati@gmail.com>
2024-09-01 12:27:17 +0530

    updated socket.js

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 // Determine the correct URL based on the environment
 const socketURL = process.env.NODE_ENV === 'production' 
-  ? 'https://syncify-s8i1.onrender.com'  // Render deployed server
+  ? 'https://syncify-h15j.onrender.com/'  // Render deployed server
   : 'http://localhost:3001';             // Local development server
 
 const socket = io(socketURL);
