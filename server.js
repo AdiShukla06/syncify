819ee95f830d5cfc6e0d7a0add327ad89e36c4f3
Aditya <aditya.vishwabharati@gmail.com>
2024-09-01 11:07:42 +0530

    updated imports format

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ import cors from 'cors';
 // Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert({
-    type: process.env.SOCKET_TYPE,
-    project_id: process.env.SOCKET_PROJECT_ID,
-    private_key_id: process.env.SOCKET_PRIVATE_KEY_ID,
-    private_key: process.env.SOCKET_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.SOCKET_CLIENT_EMAIL,
-    client_id: process.env.SOCKET_CLIENT_ID,
-    auth_uri: process.env.SOCKET_AUTH_URI,
-    token_uri: process.env.SOCKET_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.SOCKET_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.SOCKET_CLIENT_X509_CERT_URL,
+    type: String(process.env.SOCKET_TYPE),
+    project_id: String(process.env.SOCKET_PROJECT_ID),
+    private_key_id: String(process.env.SOCKET_PRIVATE_KEY_ID),
+    private_key: String(process.env.SOCKET_PRIVATE_KEY.replace(/\\n/g, '\n')),
+    client_email: String(process.env.SOCKET_CLIENT_EMAIL),
+    client_id: String(process.env.SOCKET_CLIENT_ID),
+    auth_uri: String(process.env.SOCKET_AUTH_URI),
+    token_uri: String(process.env.SOCKET_TOKEN_URI),
+    auth_provider_x509_cert_url: String(process.env.SOCKET_AUTH_PROVIDER_X509_CERT_URL),
+    client_x509_cert_url: String(process.env.SOCKET_CLIENT_X509_CERT_URL),
   })
 });
 
@@ -98,4 +98,4 @@ const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
   console.log(`Listening on *:${PORT}`);
-});
+}); 
