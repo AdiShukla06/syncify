40f4e4f868cf10edaa2a59ee09349b76d253c5d3
Aditya <aditya.vishwabharati@gmail.com>
2024-09-01 10:34:38 +0530

    updated cors

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
-import io from 'socket.io-client';
+import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
-
-export default socket;
+const socket = io(process.env.NODE_ENV === 'production' 
+    ? 'https://syncify-s8i1.onrender.com/' 
+    : 'http://localhost:3001');
